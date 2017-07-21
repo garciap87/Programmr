2e0790b1d59e5a7495669704d3f2c14cfb28271a
Phillip Garcia <garciap87@gmail.com>
2017-07-20 19:30:38 -0500

    updated code

--- Miscellaneous/QuadrantOfAngle.java
+++ Miscellaneous/QuadrantOfAngle.java
@@ -15,7 +15,6 @@ public class QuadrantOfAngle
 {
    public static void main (String[] args)
    {
-      
       int angle = 0;
       System.out.print("Type an integer angle (in degrees) and press Enter: ");
       Scanner sc = new Scanner(System.in);
@@ -23,23 +22,22 @@ public class QuadrantOfAngle
 
         int quadrant = 0;
 
-        if(angle >= 0 && angle <=90)
-        {
-            quadrant = 1;
-        }
-        else if(angle >90 && angle <=180)
-        {
-            quadrant = 2;
-        }
-        else if(angle >180 && angle <=270)
-        {
-            quadrant = 3;
-        }
-        else if(angle >270 && angle <=360)
-        {
-            quadrant = 4;
-        }
-
+		if(angle <=90)
+		{
+		    quadrant = 1;
+		}
+		else if(angle <=180)
+		{
+		    quadrant = 2;
+		}
+		else if(angle <=270)
+		{
+		    quadrant = 3;
+		}
+		else if(angle <=360)
+		{
+		    quadrant = 4;
+		}
       // output
       System.out.println("Quadrant is: " + quadrant);
       sc.close();  
